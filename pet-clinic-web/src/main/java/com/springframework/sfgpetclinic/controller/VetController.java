e4568556bbd989681a4664baa8f6962faf0e4398
Zeeshan-Jawed <jawedzeeshan1@gmail.com>
2020-05-06 02:49:43 +0500

    Added Vet List and Update OwnerController. Closes #14

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controller/VetController.java
@@ -1,12 +1,21 @@
 package com.springframework.sfgpetclinic.controller;
 
+import com.springframework.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping("/vets")
-    public String listVet(){
+    public String listVet(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vet/index";
     }
 }
