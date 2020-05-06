a64efdddf9bd931a5957cd515656a9409c7c39d7
Zeeshan-Jawed <jawedzeeshan1@gmail.com>
2020-05-06 05:42:36 +0500

    Impl Auto Generate Id. Closes #22

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -21,26 +21,22 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstname("Michael");
         owner1.setLastname("Weston");
         ownerService.save(owner1);
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstname("Fiona");
         owner2.setLastname("Glenanne");
         ownerService.save(owner2);
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstname("Sam");
         vet1.setLastname("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstname("Jessie");
         vet2.setLastname("Porter");
 
